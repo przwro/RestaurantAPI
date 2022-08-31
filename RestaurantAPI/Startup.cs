8cf5b8e341ecdcaad57e789432d54123794e20a4
wrobp0kr <przemyslaw.wrobel@walstead-ce.com>
2022-08-31 17:04:56 +0200

    Dostastowanie wstępne Startup.cs

--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -23,7 +23,7 @@ namespace RestaurantAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,15 +33,8 @@ namespace RestaurantAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -49,7 +42,7 @@ namespace RestaurantAPI
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
