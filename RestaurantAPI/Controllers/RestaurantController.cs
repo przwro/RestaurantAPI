b63dbce081e00073da9d05d32c76f9345dd96f1c
wrobp0kr <przemyslaw.wrobel@walstead-ce.com>
2022-09-05 08:52:41 +0200

    pobieranie zasobów przez kontroler

---
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/restaurant")]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly RestaurantDbContext dbContext;
+
+        public RestaurantController(RestaurantDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Restaurant>> GetAll()
+        {
+            var restuarants = dbContext.Restaurants.ToList();
+
+            return Ok(restuarants);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Restaurant> Get([FromRoute] int id)
+        {
+            var restaurant = dbContext.Restaurants.FirstOrDefault(r => r.Id == id);
+
+            if(restaurant is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(restaurant);
+        }
+    }
+}
