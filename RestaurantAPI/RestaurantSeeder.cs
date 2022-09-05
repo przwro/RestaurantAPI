de9a6c47224d82cf15a980c8ffb80b81523dccfb
wrobp0kr <przemyslaw.wrobel@walstead-ce.com>
2022-09-05 08:24:59 +0200

    Seedowanie tabel

---
+++ RestaurantAPI/RestaurantSeeder.cs
@@ -0,0 +1,96 @@
+﻿using RestaurantAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI
+{
+    public class RestaurantSeeder
+    {
+        private readonly RestaurantDbContext _dbContext;
+
+        public RestaurantSeeder(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Seed()
+        {
+            if(_dbContext.Database.CanConnect())
+            {
+                if(!_dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    _dbContext.Restaurants.AddRange(restaurants);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            var restaurants = new List<Restaurant>()
+            {
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description = "KFC (short for Kentucky Fried Chicken) is an American fast food restaurant chain headquartered",
+                    ContactEmail = "contact@kfc.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Nashville Hot Chicken",
+                            Price = 10.30M
+                        },
+
+                        new Dish()
+                        {
+                            Name = "Chicken Nuggets",
+                            Price = 5.30M
+                        }
+                    },
+                    Address = new Address()
+                    {
+                        City = "Kraków",
+                        Street = "Długa 5",
+                        PostalCode = "30-001"
+                    }
+                },
+                new Restaurant()
+                {
+                    Name = "McDonald",
+                    Category = "Fast Food",
+                    Description = "McDonald's Corporation (McDonald's)",
+                    ContactEmail = "contact@mcdonald.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Hamburger",
+                            Price = 5.10M
+                        },
+
+                        new Dish()
+                        {
+                            Name = "Cheeseburger",
+                            Price = 6.20M
+                        }
+                    },
+                    Address = new Address()
+                    {
+                        City = "Kraków",
+                        Street = "Floriańska 10",
+                        PostalCode = "33-222"
+                    }
+                }
+            };
+
+            return restaurants;
+        }
+    }
+}
